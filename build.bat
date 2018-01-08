b33854c30452c9d12b2dc5370ea7156cf7e46bb8
rcx <rcx@users.noreply.github.com>
2018-01-07 23:02:58 -0500

    Update build

---
+++ build.bat
@@ -0,0 +1,5 @@
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" /property:Platform=x64 /property:Configuration=Debug /t:Clean,Build
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" /property:Platform=x64 /property:Configuration=Debug64 /t:Clean,Build
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" /property:Platform=x64 /property:Configuration=Release /t:Clean,Build
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" /property:Platform=x64 /property:Configuration=Release64 /t:Clean,Build
+pause
